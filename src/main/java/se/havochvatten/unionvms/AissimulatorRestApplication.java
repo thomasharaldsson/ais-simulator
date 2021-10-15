1f720701b231bb5cc75f25578c2d545431c2ee05
Thomas Haraldsson <thomasharaldsson5@gmail.com>
2021-10-15 11:43:51 +0200

    Added paraya + updated pom.xml + added java files

---
+++ src/main/java/se/havochvatten/unionvms/AissimulatorRestApplication.java
@@ -0,0 +1,13 @@
+package se.consid.aiscontroller.aissimulator;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class AissimulatorRestApplication extends Application {
+}
